cea8fa60434c31919f9419a633cd3ada6c3707e0
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2025-08-05 19:57:20 +0200

    fix deprecated actions and dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,42 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default defineConfig([
   globalIgnores(['**/node_modules/', '**/dist/']),
   {
-    extends: compat.extends('xo', 'prettier'),
-
+    files: ['**/*.js'],
     languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-
       ecmaVersion: 2022,
       sourceType: 'module',
+      globals: globals.node,
     },
-
     rules: {
       'no-unused-vars': 'error',
-
       'new-cap': [
         'error',
         {
           capIsNewExceptions: ['Router'],
         },
       ],
-
       'no-implicit-coercion': 'off',
     },
   },
   {
     files: ['**/*.ts', '**/*.tsx'],
-    extends: compat.extends('xo-typescript', 'prettier'),
-
     languageOptions: {
       parser: tsParser,
-      ecmaVersion: 2022,
-      sourceType: 'module',
-
       parserOptions: {
         project: './tsconfig.json',
       },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.node,
     },
-
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     rules: {
       '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
       '@typescript-eslint/no-floating-promises': 'error',
@@ -62,7 +44,6 @@ export default defineConfig([
   },
   {
     files: ['**/model/*.ts'],
-
     rules: {
       '@typescript-eslint/naming-convention': 'off',
     },
