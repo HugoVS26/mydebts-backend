f5479cdac4e603e94394be90771844c2aa3ca123
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2025-08-16 15:22:14 +0200

    add ping router and its tests

---
+++ src/features/ping/router/pingRouter.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest';
+import '../../../server/index';
+import app from '../../../server/app';
+
+describe('Given a GET / endpoint', () => {
+  describe('When it receives a request', () => {
+    test("Then it should respond with status code 200 and the message 'Leeeeroy Jenkins!'", async () => {
+      const path = '/';
+      const expectedStatusCode = 200;
+      const expectedMessage = 'Leeeeroy Jenkins!';
+
+      const response = await request(app).get(path).expect(expectedStatusCode);
+
+      expect(response.body).toHaveProperty('message', expectedMessage);
+    });
+  });
+});
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,12 @@ import morgan from 'morgan';
 import cors from 'cors';
 import { app } from './app.js';
 import { corsOptions } from './utils/cors.js';
+import pingRouter from '../features/ping/router/pingRouter.js';
+import { generalError } from './middlewares/errors/errorsMiddleware.js';
 
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
+
+app.use('/', pingRouter);
+
+app.use(generalError);
