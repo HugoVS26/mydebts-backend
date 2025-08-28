aa61375b847659b7a05d7304d3cd2102cf7468d0
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2025-08-28 21:57:23 +0200

    add tests for debts router

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,10 +7,17 @@ import {
   notFound,
 } from './middlewares/errors/errorsMiddleware.js';
 import pingRouter from '../features/ping/router/pingRouter.js';
+import createDebtsRouter from '../features/debts/router/debtsRouter.js';
+import DebtsMongooseRepository from '../features/debts/repository/DebtsMongooseRepository.js';
+import DebtsController from '../features/debts/controller/DebtsController.js';
 
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
 
+const debtsMongooseRepository = new DebtsMongooseRepository();
+const debtsController = new DebtsController(debtsMongooseRepository);
+
+app.use('/debts', createDebtsRouter(debtsController));
 app.use('/ping', pingRouter);
 
 app.use(notFound);
