08c627a9577ec5fa8de8f9d66ab9dc0220fc50d7
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2026-01-20 18:44:25 +0100

    add auth endpoint to main index

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,23 +2,26 @@ import morgan from 'morgan';
 import cors from 'cors';
 import { app } from './app.js';
 import { corsOptions } from './utils/cors.js';
-import {
-  generalError,
-  notFound,
-} from './middlewares/errors/errorsMiddleware.js';
+import { generalError, notFound } from './middlewares/errors/errorsMiddleware.js';
 import pingRouter from '../features/ping/router/pingRouter.js';
 import createDebtsRouter from '../features/debts/router/debtsRouter.js';
 import DebtsMongooseRepository from '../features/debts/repository/DebtsMongooseRepository.js';
 import DebtsController from '../features/debts/controller/DebtsController.js';
+import createAuthRouter from '../features/auth/router/authRouter.js';
+import { AuthController } from '../features/auth/controller/AuthController.js';
+import { authMiddleware } from '../features/auth/middlewares/authMiddleware.js';
 
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
 
 const debtsMongooseRepository = new DebtsMongooseRepository();
 const debtsController = new DebtsController(debtsMongooseRepository);
+const authController = new AuthController();
 
-app.use('/debts', createDebtsRouter(debtsController));
 app.use('/ping', pingRouter);
+app.use('/auth', createAuthRouter(authController));
+
+app.use('/debts', authMiddleware, createDebtsRouter(debtsController));
 
 app.use(notFound);
 app.use(generalError);
