ddea91a287f9712904b68e5971ca9805d1b4e668
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2026-02-04 17:18:20 +0100

    add test cases for me endpoint in auth router

--- src/features/auth/router/authRouter.ts
+++ src/features/auth/router/authRouter.ts
@@ -3,6 +3,7 @@ import { AuthController } from '../controller/AuthController';
 import { registerSchema } from '../validators/request/register.schema.js';
 import { loginSchema } from '../validators/request/login.schema.js';
 import { validateRequest } from '../../../server/middlewares/validators/validateRequest.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 export default function createAuthRouter(controller: AuthController) {
   const router = Router();
@@ -10,6 +11,8 @@ export default function createAuthRouter(controller: AuthController) {
   router.post('/register', validateRequest(registerSchema), controller.register.bind(controller));
 
   router.post('/login', validateRequest(loginSchema), controller.login.bind(controller));
+
+  router.get('/me', authMiddleware, (req, res) => controller.me(req, res));
 
   return router;
 }
