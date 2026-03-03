027b79cd31b0d2288c2a3ac6afd143a9101cbf50
Hugo Veiga <141176099+HugoVS26@users.noreply.github.com>
2026-03-03 13:39:59 +0100

    debug index

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import 'dotenv/config';
-import chalk from 'chalk';
-import debugCreator from 'debug';
+
+console.log('Starting...');
+console.log('ENV CHECK:', {
+  JWT_SECRET: !!process.env['JWT_SECRET'],
+  MONGODB_URL: !!process.env['MONGODB_URL'],
+  NODE_ENV: process.env['NODE_ENV'],
+  PORT: process.env['PORT'],
+});
+
 import { startServer } from './server/app.js';
 import { connectToDatabase } from './database/index.js';
 import './server/index.js';
 import { startUpdateUnpaidToOverdueJob } from './jobs/updateUnpaidToOverdue.job.js';
 
-const debug = debugCreator('src:index');
+console.log('All imports loaded');
 
 try {
   const port = process.env['PORT'] ?? 4000;
@@ -16,9 +23,7 @@ try {
     process.exit(1);
   }
 
-  const mongoUrl = process.env['MONGODB_URL'];
-
-  await connectToDatabase(mongoUrl);
+  await connectToDatabase(process.env['MONGODB_URL']);
   startUpdateUnpaidToOverdueJob();
   startServer(+port);
 } catch (error) {
